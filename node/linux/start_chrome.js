2c874af690cd2a7f6274aa0dd39fb9c57c4a5a49
3t2ugg1e <62066043+3t2ugg1e@users.noreply.github.com>
2022-07-09 00:26:37 +0800

    Delete node directory

--- node/linux/start_chrome.js
+++
@@ -1,8 +0,0 @@
-const puppeteer = require('puppeteer');
-const process = require('process');
-
-(async () => {
-  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'], timeout:0});
-  console.log(browser.wsEndpoint());
-  
-})();
